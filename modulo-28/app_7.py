6d65e6b1b0de463a8563f5324f2978b1439c5af0
Jefferson Silva <jff.silva11@gmail.com>
2023-10-13 12:30:01 -0300

    Ajustes Path Imagens

--- modulo-28/app_7.py
+++ modulo-28/app_7.py
@@ -48,7 +48,7 @@ def to_excel(df):
 def main():
     # Configuração inicial da página da aplicação
     st.set_page_config(page_title = 'Telemarketing analisys', \
-        page_icon = 'telmarketing_icon.png',
+        page_icon = 'https://github.com/jeffersonsilva11/Cientista-de-Dados/blob/main/modulo-28/telmarketing_icon.png',
         layout="wide",
         initial_sidebar_state='expanded'
     )
@@ -58,7 +58,7 @@ def main():
     st.markdown("---")
     
     # Apresenta a imagem na barra lateral da aplicação
-    image = Image.open("Bank-Branding.jpg")
+    image = Image.open("https://github.com/jeffersonsilva11/Cientista-de-Dados/blob/main/modulo-28/Bank-Branding.jpg")
     st.sidebar.image(image)
 
     # Botão para carregar arquivo na aplicação

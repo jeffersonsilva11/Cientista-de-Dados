959477c22830cf1f70a4ad5de5a6766694709b21
Jefferson Silva <jff.silva11@gmail.com>
2023-10-17 10:49:07 -0300

    Tarefa 2 Modulo 31 v3

--- modulo-31/app_m31.py
+++ modulo-31/app_m31.py
@@ -1,5 +1,10 @@
-import pandas as pd
-import streamlit as st
+import pandas            as pd
+import streamlit         as st
+import numpy             as np
+
+from datetime            import datetime
+from PIL                 import Image
+from io                  import BytesIO
 
 # Função para o upload do arquivo
 def user_upload():
